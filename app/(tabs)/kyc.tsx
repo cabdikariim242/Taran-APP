cfd87637c626f8cfc58582919ac65c0e29be9e29
Cabdikariim <cabdikariimmaxamaddaahir@gmail.com>
2025-10-18 14:56:50 +0300

    set up finished

---
+++ app/(tabs)/kyc.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function kyc() {
+  return (
+    <View>
+      <Text>kyc</Text>
+    </View>
+  )
+}
---
+++ assets/images.ts
@@ -0,0 +1,9 @@
+// @ts-ignore: missing type declaration for png files
+import profile from '../assets/profile.png';
+// @ts-ignore: missing type declaration for png files
+import notification from '../assets/notification.png';
+
+export const images = {
+  profile,
+  notification,
+};
