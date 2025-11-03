a1bf3db9bead7dddcf25f8e9da5cd749213f1097
Cabdikariim <cabdikariimmaxamaddaahir@gmail.com>
2025-11-03 13:44:44 +0300

    swap accounts and kyc is finished and made scrollable

--- app/(tabs)/accounts.tsx
+++ app/(tabs)/accounts.tsx
@@ -1,10 +1,102 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { images } from "@/assets/images";
+import React from "react";
+import {
+  Image,
+  ImageSourcePropType,
+  Pressable,
+  ScrollView,
+  Text,
+  View,
+} from "react-native";
 
 export default function accounts() {
   return (
-    <View className="flex-1 justify-center items-center bg-white"> 
-      <Text  className=" font-bold text-md text-center text-red-300">ACCOUNTS</Text>
+    <View className="flex-1 justify-start pt-4 items-center bg-[#ffffff84]">
+      {/* my accounts and add acount button */}
+      <View className="flex-row justify-center gap-52 border-b border-[#0C4C7B1A] pb-4 items-center w-full mx-auto ">
+        <Text className="font-bold text-[#0C4C7B] text-[18px] ">
+          {" "}
+          My Accounts
+        </Text>
+        <Pressable className="flex-row gap-2">
+          <Image source={images.AddButton} className="w-[15px] h-[15px] " />
+          <Text className="font-bold text-[#0C4C7B] text-[12px] ">
+            Add Account
+          </Text>
+        </Pressable>
+      </View>
+      {/* Cards */}
+      <View className="w-full mt-4">
+        <ScrollView className="w-full h-[560px]">
+          <Cards
+            Icon={images.EVCPULS}
+            service="Evc Plus Service"
+            user="Ridwan Muse Ahmed"
+            number="******5166"
+          />
+          <Cards
+            Icon={images.Premieri}
+            service="Premier Wallet"
+            user="Ridwan Muse Ahmed"
+            number="******5166"
+          />
+          <Cards
+            Icon={images.Bybit}
+            service="Bybit Wallet Address"
+            user="USDT BEP-20"
+            number="48394u83...ds884334"
+          />
+          <Cards
+            Icon={images.YiksiWallet}
+            service="Yiksi Wallet Address"
+            user="USDT BEP-20"
+            number="48394u83...ds884334"
+          />
+          <Cards
+            Icon={images.TrustWallet}
+            service="Trust Wallet Address "
+            user="USDT TRC-20"
+            number="48394u83...ds884334"
+          />
+          <Cards
+            Icon={images.BNB}
+            service="BNB Gas Fee"
+            user="BNB"
+            number="48394u83...ds884334"
+          />
+          <Cards
+            Icon={images.BNB}
+            service="BNB Gas Fee"
+            user="BNB"
+            number="48394u83...ds884334"
+          />
+        </ScrollView>
+      </View>
     </View>
-  )
+  );
 }
+
+interface CardProps {
+  Icon: ImageSourcePropType;
+  service: string;
+  user: string;
+  number: string;
+}
+const Cards = ({ Icon, service, user, number }: CardProps) => {
+  return (
+    <View className="bg-white w-[355px] my-4 h-[80px] mx-auto rounded-[12px] flex-row justify-between items-center ">
+      <View className=" flex-row gap-2 w-[161px] h-[80px] justify-center items-center">
+        <Image source={Icon} className="w-[40px] h-[40px]" />
+        <View className="">
+          <Text className="font-bold text-[#0C4C7B] text-[13px] ">
+            {service}
+          </Text>
+          <Text className="font-bold text-[#0C4C7B] text-[10px] ">{user}</Text>
+        </View>
+      </View>
+      <Text className="font-[400]  w-[88px] h-[14px]   text-[#0C4C7B] text-[14px] mr-4">
+        {number}
+      </Text>
+    </View>
+  );
+};
--- assets/images.ts
+++ assets/images.ts
@@ -9,6 +9,19 @@ import bg from '@/assets/bg.png'
 import Award from './award taran Swap-01 1.png'
 import swapIcon from './swapIcon.png'
 import BorderBottom from './Line 26.png'
+import AddButton from './AddButton.png'
+import EVCPULS from './EVCPULS.png'
+import Premieri from './premieriwallet.png'
+import Bybit from './bybit.png'
+import TrustWallet from './trustWallet.png'
+import YiksiWallet from './yikisWallet.png'
+import BNB from './bnb.png'
+import Right from './XMLID_225_.png'
+import verified from './verified.png'
+import Goverment from './goverment.png'
+import Face from './face.png'
+import Resident from './resident.png'
+import Certification from './certification.png'
 export const images = {
   profile,
   notification,
@@ -21,5 +34,17 @@ export const images = {
   Award,
   swapIcon,
   BorderBottom,
-  
+  AddButton,
+  EVCPULS,
+  Premieri,
+  BNB,
+  Bybit,
+  YiksiWallet,
+  TrustWallet,
+  Right,
+  verified,
+  Goverment,
+  Face,
+  Resident,
+  Certification
 };
